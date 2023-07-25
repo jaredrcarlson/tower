a051d25166ada1842f094a82997a929e015ded78
Jared Carlson <jaredrcarlson@gmail.com>
2023-07-25 16:07:14 -0600

    first commit

---
+++ tower.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-eux5v4apdr8nm4hz.us.auth0.com'
+export const audience = 'classroom2'
+export const clientId = 'Rur2HYRUEOc9CFa5kFMGh67L2LmsuzQH'
