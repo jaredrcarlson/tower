cc114a9cade3f872133258969b0197a2c6055d42
Jared Carlson <jaredrcarlson@gmail.com>
2023-09-22 15:35:23 -0600

    auth updates

--- tower.client/src/env.js
+++ tower.client/src/env.js
@@ -3,5 +3,5 @@ export const baseURL = dev ? 'http://localhost:3000' : ''
 export const servePath = dev ? '' : '/tower/'
 export const useSockets = false
 export const domain = 'dev-eux5v4apdr8nm4hz.us.auth0.com'
-export const audience = 'classroom2'
-export const clientId = 'Rur2HYRUEOc9CFa5kFMGh67L2LmsuzQH'
+export const audience = 'https://jaredcarlson.dev'
+export const clientId = 'IJ7uCpAhoRnKxuddfraRi2Mry1JmDCTP'
